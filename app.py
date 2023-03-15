100f7262eb10ac132406bde1b9321a83625fb2c3
Mischa Dhar <mischadhar94@gmail.com>
2023-03-15 17:37:27 +0000

    new charts pt1

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ import plotly.express as px
 
 st.image('data/prello.png',width=450)
 
+###############################################################################
+################### Bubble chart for department & metrics #####################
+###############################################################################
+
 st.markdown('''
 ## Bubble chart showing relationship between department & metrics of interest
 ''')
@@ -56,3 +60,34 @@ if option == 'Secondary home rate':
     fig.update_xaxes(tickangle=45)
 
 st.plotly_chart(fig)
+
+###############################################################################
+################### Bubble chart for department & metrics #####################
+###############################################################################
+
+@st.cache
+def load_yoy_df():
+    df = pd.read_csv('data/yoy_sales_growth.csv')
+    return df
+
+yoy_sales_df = load_yoy_df()
+
+yoy_sales_df.sort_values(by='yoy_sales_growth', ascending=False).head(10)
+
+fig2 = px.bar(yoy_sales_df, x="department_name"
+              , y="yoy_sales_growth"
+              , orientation='h')
+fig2.update_layout(
+    title="Secondary home rate by department<br><sup>Bubble size indicates sales price per m2</sup><br>",
+    xaxis_title="Department",
+    yaxis_title="Secondary home rate",
+    font=dict(
+        family="Courier New, monospace",
+        size=15,
+        color="RebeccaPurple"
+    ))
+
+# ax.set_title('Top 10 Departments with the Biggest Sales Value Increase', fontsize=16)
+# ax.set_xlabel('Mean Percentage Change in Sales Growth', fontsize=14)
+# ax.set_ylabel('Department', fontsize=14)
+# plt.show()
