9e491d6381deb7919829a1707f5e180ddb1a1223
Mischa Dhar <mischadhar94@gmail.com>
2023-03-15 18:00:33 +0000

    end of day streamlit

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ if option == 'Sale price per m2':
     yaxis_title="Sales price per m2",
     font=dict(
         family="Courier New, monospace",
-        size=15,
+        size=12,
         color="RebeccaPurple"
     ))
     fig.update_xaxes(tickangle=45)
@@ -54,38 +54,9 @@ if option == 'Secondary home rate':
     yaxis_title="Secondary home rate",
     font=dict(
         family="Courier New, monospace",
-        size=15,
+        size=12,
         color="RebeccaPurple"
     ))
     fig.update_xaxes(tickangle=45)
 
 st.plotly_chart(fig)
-
-###############################################################################
-################### Year on year sales growth by department ###################
-###############################################################################
-
-@st.cache
-def load_yoy_df():
-    df = pd.read_csv('data/yoy_sales_growth.csv')
-    return df
-
-yoy_sales_df = load_yoy_df()
-
-yoy_sales_df.sort_values(by='yoy_sales_growth', ascending=False).head(10)
-
-fig2 = px.bar(yoy_sales_df, x="department_name"
-              , y="yoy_sales_growth"
-              , orientation='h')
-fig2.update_layout(
-    title="Top 10 Departments with the Biggest Sales Value Increase",
-    xaxis_title="Mean Percentage Change in Sales Growth",
-    yaxis_title="Department",
-    font=dict(
-        family="Courier New, monospace",
-        size=15,
-        color="RebeccaPurple"
-    ))
-fig2.update_xaxes(tickangle=45)
-
-st.plotly_chart(fig2)
--- pages/page1.py
+++ pages/page1.py
@@ -6,6 +6,8 @@ import plotly.express as px
 ################### Year on year sales growth by department ###################
 ###############################################################################
 
+st.write("""# Yearly sales growth by department""")
+
 @st.cache
 def load_yoy_df():
     df = pd.read_csv('data/yoy_sales_growth.csv')
@@ -13,20 +15,38 @@ def load_yoy_df():
 
 yoy_sales_df = load_yoy_df()
 
-yoy_sales_df.sort_values(by='yoy_sales_growth', ascending=False).head(10)
+yoy_sales_df_head = yoy_sales_df.sort_values(by='yoy_sales_growth', ascending=False).head(10)
 
-fig2 = px.bar(yoy_sales_df, x="department_name"
-              , y="yoy_sales_growth"
-              , orientation='h')
+fig2 = px.bar(yoy_sales_df_head, x="department_name"
+              , y="yoy_sales_growth")
 fig2.update_layout(
     title="Top 10 Departments with the Biggest Sales Value Increase",
-    xaxis_title="Mean Percentage Change in Sales Growth",
-    yaxis_title="Department",
+    xaxis_title="Department",
+    yaxis_title="Avg yearly sales growth (%)",
     font=dict(
         family="Courier New, monospace",
-        size=15,
+        size=12,
         color="RebeccaPurple"
     ))
 fig2.update_xaxes(tickangle=45)
+fig2.update_layout(yaxis_range=[0,3])
 
 st.plotly_chart(fig2)
+
+yoy_sales_df_tail = yoy_sales_df.sort_values(by='yoy_sales_growth', ascending=False).tail(10)
+
+fig3 = px.bar(yoy_sales_df_tail, x="department_name"
+              , y="yoy_sales_growth")
+fig3.update_layout(
+    title="Bottom 10 Departments with the Biggest Sales Value Increase",
+    xaxis_title="Department",
+    yaxis_title="Avg yearly sales growth (%)",
+    font=dict(
+        family="Courier New, monospace",
+        size=12,
+        color="RebeccaPurple"
+    ))
+fig3.update_xaxes(tickangle=45)
+fig3.update_layout(yaxis_range=[0,3])
+
+st.plotly_chart(fig3)
