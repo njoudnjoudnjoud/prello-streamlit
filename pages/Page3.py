4c0b918d8f604a4e8cdee499c5e2e91b8ec459a4
Mischa Dhar <mischadhar94@gmail.com>
2023-03-16 10:23:37 +0000

    fixed imports

--- pages/Page3.py
+++ pages/Page3.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-from streamlit_extras.colored_header import colored_header
 
 ###############################################################################
 ##################### Sale value increases (yearly) ###########################
