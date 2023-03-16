be03f3fe72106a29ad1ccf2f9c919cc49584fa59
Mischa Dhar <mischadhar94@gmail.com>
2023-03-16 10:47:30 +0000

    fixed page 3 y axis scale

--- pages/Page3.py
+++ pages/Page3.py
@@ -17,10 +17,11 @@ df = get_df()
 df = df[df.index != 'Jura']
 df['average_increase'] = df[['2017','2018','2019','2020','2021']].mean(axis=1)
 
+df.sort_values(by='average_increase',inplace=True, ascending=False)
+
 ##################### Top 10 ###########################
 
 df_top10 = df.head(10)
-df_top10.sort_values(by='average_increase',inplace=True, ascending=False)
 
 fig1 = px.bar(df_top10, df_top10['department_name'], 'average_increase')
 fig1.update_layout(
@@ -30,7 +31,7 @@ fig1.update_layout(
         family="Courier New, monospace",
         size=13,
         color="RebeccaPurple"),
-    yaxis_range=[0,150_000_000])
+    yaxis_range=[0,200_000_000])
 fig1.update_xaxes(tickangle=45)
 
 
@@ -40,7 +41,6 @@ st.plotly_chart(fig1)
 ##################### Bottom 10 ###########################
 
 df_bottom10 = df.tail(10)
-df_bottom10.sort_values(by='average_increase',inplace=True, ascending=False)
 
 fig2 = px.bar(df_bottom10, df_bottom10['department_name'], 'average_increase')
 fig2.update_layout(
@@ -50,8 +50,10 @@ fig2.update_layout(
         family="Courier New, monospace",
         size=13,
         color="RebeccaPurple"),
-    yaxis_range=[0,150_000_000])
+    yaxis_range=[0,10_000_000])
 fig2.update_xaxes(tickangle=45)
 
 st.write("""#### Bottom 10 Department by average yearly sales value increases from 2017 to 2021""")
+st.write("""##### Please note difference in y-axis scale""")
+
 st.plotly_chart(fig2)
